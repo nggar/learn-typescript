19513117c8db552843ff20f451ac48e999686a8d
nggar <enggarjpx@gmail.com>
2022-08-09 22:02:42 +0700

    implement interface to user and company class to help find error

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,11 +1,11 @@
 // Intructions to every other class on how they can be an argument to 'addMarker'
-interface Mappable {
+export interface Mappable {
     location: {
         lat: number;
         lng: number;
     };
-
     markerContent(): string;
+    color: string;
 }
 
 export class CustomMap {
