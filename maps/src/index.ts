389c30892fa7ca2c13c6d94fc20934e14772dfd9
nggar <enggarjpx@gmail.com>
2022-08-09 11:44:49 +0700

    display google map

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,9 +1,10 @@
 import { User } from './User';
 import { Company } from './Company';
 
-const user = new User();
-const company = new Company();
-console.log(user);
-console.log(company);
+const mapDiv: HTMLElement | null = document.getElementById('map');
 
-google;
+mapDiv &&
+    new google.maps.Map(mapDiv, {
+        zoom: 1,
+        center: { lat: 0, lng: 0 },
+    });
